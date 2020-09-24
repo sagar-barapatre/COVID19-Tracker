8c76f53ab564e407bbcaf27347e01dd7cc0d09db
Sagar Barapatre <b119016@iiit-bh.ac.in>
2020-09-24 08:58:45 +0530

    Add files via upload

---
+++ build/precache-manifest.59030f8bade48a9f19b4d1e20d6d6689.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6d09bb969c72e130e15db73872e64f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e7ebffa54155a2b75fd",
+    "url": "/static/css/main.73ee34dd.chunk.css"
+  },
+  {
+    "revision": "bb3a8ba40f098dafc04a",
+    "url": "/static/js/2.dfdb2bba.chunk.js"
+  },
+  {
+    "revision": "61e25812e7f326803ad264375c765013",
+    "url": "/static/js/2.dfdb2bba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e7ebffa54155a2b75fd",
+    "url": "/static/js/main.d53fcf54.chunk.js"
+  },
+  {
+    "revision": "f175251ec165c8b54a1c",
+    "url": "/static/js/runtime-main.e52bfea7.js"
+  },
+  {
+    "revision": "1c8895dd79172b2f85043bf51fd5c7fa",
+    "url": "/static/media/01.1c8895dd.svg"
+  },
+  {
+    "revision": "4e94bdfbeca8646b61a50920291ffaad",
+    "url": "/static/media/02.4e94bdfb.svg"
+  },
+  {
+    "revision": "046acf9af57fab1131779a6afc365f0c",
+    "url": "/static/media/03.046acf9a.svg"
+  },
+  {
+    "revision": "577bf8834560303fc0ebc011d8334d04",
+    "url": "/static/media/04.577bf883.svg"
+  },
+  {
+    "revision": "6a1ae40a1db62466bd6bbda2cd4e581a",
+    "url": "/static/media/05.6a1ae40a.svg"
+  },
+  {
+    "revision": "205f578ca91d90b22afa873d2faf3b5a",
+    "url": "/static/media/06.205f578c.svg"
+  }
+]);
